40858c13fa1fb48fa7efd1fd6e687e58ac341ef5
Thawatchai Kunchaiprasert <66011270@kmitl.ac.th>
2024-07-26 12:23:22 +0700

    add multiple changes

---
+++ calculator1.py
@@ -0,0 +1,12 @@
+def add(x,y) :
+    return x+y 
+def subtract(x,y) :
+    return x-y
+def multiply(x,y) :
+    return x*y
+def divide(x,y) :
+    return x / y
+print(add(5,5))
+print(subtract(5,5))
+print(multiply(5,5))
+print(divide(5,5))
---
+++ lambda_calculator.py
@@ -0,0 +1,12 @@
+add = lambda x, y: x + y
+subtract = lambda x, y: x - y
+multiply = lambda x, y: x * y
+divide = lambda x, y: x / y
+
+x = int(7)
+y = int(5)
+print("Lambda Calculator")
+print(add(x, y))
+print(subtract(x, y))
+print(multiply(x, y))
+print(divide(x, y))
