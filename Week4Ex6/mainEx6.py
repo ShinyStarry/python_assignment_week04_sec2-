c58a5963beddce01ba38d0756f4637617b515419
Thawatchai Kunchaiprasert <66011270@kmitl.ac.th>
2024-07-26 13:11:26 +0700

    add changes

--- Week4Ex6/mainEx6.py
+++ Week4Ex6/mainEx6.py
@@ -4,6 +4,6 @@ sample_string = "hello World"
 
 print("Original:", sample_string)
 print("Reversed:", so.reverse_string(sample_string))
-print("Capitalized:", so.capitalize_string(sample_string))
+print("Capitalized:", so.capitalize_string(sample_string)) 
 print("Lowercase:", so.lowercase_string(sample_string))
 print("Uppercase:", so.uppercase_string(sample_string))
